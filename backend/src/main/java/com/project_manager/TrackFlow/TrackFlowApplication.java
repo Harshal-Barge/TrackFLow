87f21f3b5197faa863ca52409a45559eb864ce12
Harshal Barge <harshalbarge11@gmail.com>
2025-01-05 01:00:45 +0530

    Setting up frontend and backend

---
+++ backend/src/main/java/com/project_manager/TrackFlow/TrackFlowApplication.java
@@ -0,0 +1,13 @@
+package com.project_manager.TrackFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackFlowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project_manager/TrackFlow/TrackFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_manager.TrackFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
