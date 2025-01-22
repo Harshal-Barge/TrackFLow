dac08f676caa39aa6f03c7441e085d6ca7b62b15
Harshal Barge <harshalbarge11@gmail.com>
2025-01-22 20:09:47 +0530

    Changed Required to False for Get Project EndPoint

--- backend/src/main/java/com/project_manager/TrackFlow/controller/ProjectController.java
+++ backend/src/main/java/com/project_manager/TrackFlow/controller/ProjectController.java
@@ -33,8 +33,8 @@ public class ProjectController {
 
     @GetMapping("/getProjects")
     public ResponseEntity<List<Project>> getProjects(
-            @RequestParam String category,
-            @RequestParam String tag,
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String tag,
             @RequestHeader("Authorization") String jwt
     ) throws Exception {
         User user = userService.findUserProfileByJwt(jwt);
