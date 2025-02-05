7ad76f6f4bf1fa992e81426dc3e42a014f777577
Harshal Barge <harshalbarge11@gmail.com>
2025-02-05 15:57:21 +0530

    Implemented ProjectDetails page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Home } from './pages/Home'
 import { Navbar } from './pages/Navbar/Navbar'
+import { ProjectDetails } from './pages/Project/ProjectDetails'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/project/:id' element={<ProjectDetails />} />
+      </Routes >
     </>
   )
 }
---
+++ frontend/src/pages/Project/ProjectDetails.jsx
@@ -0,0 +1,82 @@
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTrigger } from '@/components/ui/dialog'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { PlusIcon } from '@radix-ui/react-icons'
+import React from 'react'
+import { InviteUserForm } from './InviteUserForm'
+import { IssueList } from '../Issue/IssueList'
+import { ChatBox } from '../Chat/ChatBox'
+
+export const ProjectDetails = () => {
+    const handleProjectInvitation = () => {
+
+    }
+
+    return (
+        <>
+            <div className='mt-5 lg:px-10'>
+                <div className='lg:flex gap-5 justify-between pb-4'>
+                    <ScrollArea className='h-screen lg:w-[69%] pr-2'>
+                        <div className='text-gray-400 pb-10 w-full'>
+                            <h1 className='text-lg font-semibold pb-5'>Create Ecommerce Website</h1>
+                            <div className='space-y-5 pb-10 text-sm'>
+                                <p className='w-full md:max-w-lg lg:max-w-xl'>
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                                    Quaerat facilis corporis quisquam earum expedita dolor,
+                                    quod nisi tempora accusantium ipsum atque distinctio quasi
+                                </p>
+                                <div className='flex'>
+                                    <p className='w-36'>Project Lead :</p>
+                                    <p>Name</p>
+                                </div>
+                                <div className='flex'>
+                                    <p className='w-36'>Members :</p>
+                                    <div className='flex items-center'>
+                                        {[1, 1, 1].map((item) => <Avatar className='cursor-pointer mx-1' key={item}>
+                                            <AvatarFallback>U</AvatarFallback>
+                                        </Avatar>)}
+                                    </div>
+                                    <Dialog>
+                                        <DialogTrigger>
+                                            <DialogClose>
+                                                <Button className='mx-1' size='sm' variant='outline' onClick={handleProjectInvitation}>
+                                                    <span>invite</span>
+                                                    <PlusIcon className='w-3 h-3' />
+                                                </Button>
+                                            </DialogClose>
+                                        </DialogTrigger>
+                                        <DialogContent>
+                                            <DialogHeader>Invite User</DialogHeader>
+                                            <InviteUserForm />
+                                        </DialogContent>
+                                    </Dialog>
+                                </div>
+                                <div className='flex'>
+                                    <p className='w-36'>Category :</p>
+                                    <p>Fullstack</p>
+                                </div>
+                                <div className='flex'>
+                                    <p className='w-36'>Status :</p>
+                                    <Badge>Open</Badge>
+                                </div>
+                            </div>
+                            <section>
+                                <p className='py-5 border-b text-lg -tracking-wider'>Tasks</p>
+                                <div className='lg:flex md:flex gap-3 justify-between py-5'>
+                                    <IssueList status='pending' title='Todo List' />
+                                    <IssueList status='in-progress' title='task2' />
+                                    <IssueList status='done' title='task2' />
+                                </div>
+                            </section>
+                        </div>
+                    </ScrollArea>
+                    <div>
+                        <ChatBox />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
